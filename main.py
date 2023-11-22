776a576508d2fc03123462babae533eccbfca841
Vesztergom Adam <vesztergom85@gmail.com>
2023-11-22 20:19:42 +0100

    teszt22

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
 
+
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,3 +16,27 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+hello = "Hello World"
+
+print(hello)
+
+
+
+
+class Cat:
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+    def printCat(self):
+        print("Name:", self.name, "Age:", self.age, "Color:", self.color)
+
+cat1 = Cat("Tom", 5, "Blue")
+
+cat1.printCat()
+
+
+def cat():
+    return None
